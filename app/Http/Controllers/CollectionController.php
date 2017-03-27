95f425f073f466e2eedf7f61886d9743fa7e30d0
Nnabueze Opara <villavelle101@gmail.com>
2017-03-27 11:28:50 +0100

    corrected error on agency collection

--- app/Http/Controllers/CollectionController.php
+++ app/Http/Controllers/CollectionController.php
@@ -151,11 +151,15 @@ class CollectionController extends Controller
         //getting collection within the date range
         $collections = Collection::where("mda_id",$mda_id)->whereDate('created_at',">=",$start_date )->whereDate('created_at',"<=",$end_date )->get();
 
+        //getting the name of the search MDA
+        $mda = Mda::find($mda_id);
+        $mda_name = $mda->mda_name;
+
         //select station base on MDA
         
         if (count($collections) > 0) {
                 
-            return view("collection.angency_range",compact("igr","sidebar","collections"));
+            return view("collection.angency_range",compact("igr","sidebar","collections","mda_name"));
         }
 
             Session::flash("warning","Failed! No result found.");
