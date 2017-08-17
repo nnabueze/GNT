51ec7a60c6aae3e3b6b5a7cc2f13f92be0598f0d
Opara Nnabueze <villavelle101@gmail.com>
2017-08-17 02:39:15 +0100

    added total monthly colllection

--- app/Http/Controllers/IgrMobileController.php
+++ app/Http/Controllers/IgrMobileController.php
@@ -19,6 +19,7 @@ use App\Postable;
 use App\Subhead;
 use App\Remittance;
 use App\Collection;
+use App\Igr;
 use Carbon\Carbon;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
@@ -32,26 +33,76 @@ class IgrMobileController extends Controller
     {
     	$credentials = $request->only('email', 'password');
 
-    	try{
-    		if (! $token = JWTAuth::attempt($credentials)) {
-                return $this->response->errorUnauthorized();
-            }
+        $data = array();
 
-    	} catch (JWTException $e) {
-    		return $this->response->errorInternal();
-    	}
-    	
-        return $this->response->array(compact('token'))->setStatusCode(200);
+        try{
+            if (! $token = JWTAuth::attempt($credentials)) {
+                return $this->response->errorUnauthorized();
+            }  
+
+        } catch (JWTException $e) {
+              return $this->response->errorInternal();
+        }
+
+        if (Auth::attempt(['email' => $request->input("email"), 'password' => $request->input("password")])) {
+
+            $firstday_last_date = date('Y-m-d', strtotime('first day of last month'));
+            $lastday_last_date = date('Y-m-d', strtotime('last day of last month'));
+            $firstday_curent_date = date('Y-m-d', strtotime(date('Y-m-1')));
+            $yestarday = date('Y-m-d',strtotime("-1 days"));
+            $today = date('Y-m-d');
+
+            $igr = Igr::with("mdas")->find(Auth::user()->igr_id);
+
+            $data['last_month'] = 0;
+            $data['current_month'] = 0;
+            $data['yestarday'] = 0;
+            $data['today'] = 0;
+
+            foreach ($igr->mdas as $mda) {
+
+                $last_months = Collection::where("mda_id",$mda->id)->where("created_at",">=", $firstday_last_date)
+                                ->where("created_at","<=",$lastday_last_date)->get();
+
+                if (count($last_months) > 0) {
+                    foreach ($last_months as $collection) {
+
+                        $data['last_month'] += $collection->amount;
+                    }
+                }
+
+
+                $current_months = Collection::where("mda_id",$mda->id)->where("created_at",">=", $firstday_curent_date)->get();
+                if (count($current_months) > 0) {
+                    foreach ($current_months as $current_month) {
+
+                        $data['current_month'] += $current_month->amount;
+                    }
+                }
+
+                $yestarday_date = Collection::where("mda_id",$mda->id)->where("created_at", $yestarday)->get();
+                if (count($yestarday) > 0) {
+                    foreach ($yestarday_date as $yestarday_date) {
+
+                        $data['yestarday'] += $yestarday_date->amount;
+                    }
+                }
+
+                $today_date = Collection::where("mda_id",$mda->id)->where("created_at", $today)->get();
+                if (count($today_date) > 0) {
+                    foreach ($today_date as $today_date) {
+
+                        $data['today'] += $today_date->amount;
+                    }
+                }
+            }
+        }
+
+        $data['token'] = $token;
+        $data['igr_key'] = $igr->igr_key;
+        $data['state_name'] = $igr->state_name;
+        $data['logo'] = $igr->logo;
+
+        return $this->response->array(compact('data'))->setStatusCode(200);
     }
-
-/*        public function authentication(Request $request)
-    {
-
-            if (! Auth::attempt(['email' => $request->input("email"), 'password' => $request->input("password")])) {
-                return $this->response->errorUnauthorized();
-            }
-
-        return $request->input("password");
-        //return $this->response->array(compact('token'))->setStatusCode(200);
-    }*/
 }
