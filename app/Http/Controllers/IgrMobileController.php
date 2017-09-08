4400fe9b25a458145d737a5babd090bae142c7c5
Opara Nnabueze <villavelle101@gmail.com>
2017-09-08 10:07:51 +0100

    edit date

--- app/Http/Controllers/IgrMobileController.php
+++ app/Http/Controllers/IgrMobileController.php
@@ -255,7 +255,7 @@ class IgrMobileController extends Controller
 
 
 
-            $yestarday_date = Collection::where("mda_id",$mda->id)->where("collection_type","pos")->whereDate("created_at", $yestarday)->get();
+            $yestarday_date = Collection::where("mda_id",$mda->id)->where("collection_type","pos")->whereDate("created_at","=", $yestarday)->get();
             if (count($yestarday_date) > 0) {
                 foreach ($yestarday_date as $yestarday_date) {
 
